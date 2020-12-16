0cb76ce97bee8dfca50fc6f750ea1dbeead1e62a
berkekandemir <33724354+berkekandemir@users.noreply.github.com>
2020-12-17 01:41:41 +0300

    Add files via upload

---
+++ Fructus/View/FruitNutrientsView.swift
@@ -0,0 +1,55 @@
+//
+//  NutrientsView.swift
+//  Fructus
+//
+//  Created by Berke Can Kandemir on 12.12.2020.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    // MARK: - PROPERTIES
+    
+    var fruit: Fruit
+    let nutrients: [String] = ["Energy", "Sugar", "Fat", "Protein", "Vitamins", "Minerals"]
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        GroupBox() {
+            DisclosureGroup("Nutritional value per 100g") {
+                ForEach(0..<nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 2)
+                    
+                    
+                    HStack {
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(Font.system(.body).bold())
+                        
+                        Spacer(minLength: 25)
+                        
+                        Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
+                    } //: HStack
+                }
+            }
+            
+        } //: BOX
+        
+    }
+}
+
+// MARK: - PREVIEW
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView(fruit: fruitsData[0])
+            .preferredColorScheme(.dark)
+            .previewLayout(.fixed(width: 375, height: 440))
+            .padding()
+    }
+}
