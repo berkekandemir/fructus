db82b1815bb11e8f4d9d2c9922a815bc2ca9a447
berkekandemir <33724354+berkekandemir@users.noreply.github.com>
2020-12-17 01:51:16 +0300

    Add files via upload

---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,114 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Berke Can Kandemir on 12.12.2020.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PORPERTIES
+    
+    @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    // MARK: - SECTION 1
+                    
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                            
+                        } //: HSTACK
+                    }
+                    
+                    // MARK: - SECTION 2
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                    } //: BOX
+                    
+                    // MARK: - SECTION 3
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                    ) {
+                        
+                        SettingsRowView(name: "Developer", content: "Berke Can Kandemir")
+                        SettingsRowView(name: "Designer", content: "Berke Can Kandemir")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14.x")
+                        SettingsRowView(name: "Website", linkLabel: "LinkedIn", linkDestination: "www.linkedin.com/in/berke-can-kandemir/")
+                        SettingsRowView(name: "Github", linkLabel: "berkekandemir", linkDestination: "github.com/berkekandemir")
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                        
+                    } //: BOX
+                    
+                } //: VStack
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing:
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                        Image(systemName: "xmark")
+                        } //: BUTTON
+                )
+                .padding()
+            } //: SCROLL
+        } //: NAVIGATION
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .preferredColorScheme(.dark)
+    }
+}
