6d1a3991dbf8dda07f7d1cfa20bb52d2d10944a4
berkekandemir <33724354+berkekandemir@users.noreply.github.com>
2020-12-17 01:39:21 +0300

    Add files via upload

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,35 @@
+//
+//  OnboaringView.swift
+//  Fructus
+//
+//  Created by Berke Can Kandemir on 11.12.2020.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(fruits.shuffled()[0...5]) { item in
+                FruitCardView(fruit: item)
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(fruits: fruitsData)
+            .previewDevice("iPhone 11")
+    }
+}
